7186cf66e19e0c2802f789e19c5ed700be64a6c4
guta307 <veiga187@gmail.com>
2022-12-22 09:39:51 -0300

    commit

--- splitfiles.py
+++ splitfiles.py
@@ -1,14 +1,13 @@
-import nltk
+
 import os
 import pandas
 import pdfplumber
-import fileinput
+from charset_normalizer import md__mypyc
 import tkinter as tk     # from tkinter import Tk for Python 3.x
+from tkinter import simpledialog
 from tkinter.filedialog import askopenfilename,askdirectory
 from PyPDF2 import PdfFileWriter, PdfFileReader
-from nltk import ne_chunk, pos_tag, word_tokenize, sent_tokenize
-from nltk.tree import Tree
-import time
+
 
 df= pandas.read_excel('./Pasta1.xlsx',usecols = 'B')
 
@@ -34,14 +33,15 @@ def pdf_get_name (page, pdf_file):
               for index,row in df.iterrows():
                 if row['FUNCIONÁRIO'].lower() in text.lower():
                   name=row['FUNCIONÁRIO'] 
- 
   print(name)
+  if name == '':
+    name = 'nome_desconhecido ['+str(page)+']'
   return name
   
 
 def pdf_sep (pdf_file, out_dir):
   '''
-  pdf_file é o caminho do pdf orinal
+  pdf_file é o caminho do pdf original
   out_dir é a pasta onde os pdfs divididos serão salvos
   '''
   #Abre o pdf original no modo de leitura
@@ -56,13 +56,14 @@ def pdf_sep (pdf_file, out_dir):
     
     #Faz uma iteração para cada uma das páginas
     for page in range(num_pages):
+      
       pdf_writer = PdfFileWriter()
       #Adiciona a página da iteração atual ao objeto para escrita do PDF
       pdf_writer.addPage(pdf_content.getPage(page))
       
       #Invoca a função pdf_get_name para extrair o nome contido na página atual
       pdf_name = pdf_get_name(page,pdf_file)
-
+    
       #O médoto os.path.join() une o caminho para gravação, o nome e a extesão do arquivo pdf. 
       pdf_out = os.path.join(out_dir, pdf_name +'.pdf')
 
@@ -79,8 +80,8 @@ def pdf_sep (pdf_file, out_dir):
 root = tk.Tk()
 root.withdraw() # we don't want a full GUI, so keep the root window from appearing
 path = askopenfilename(defaultextension=".pdf",filetypes=[('pdf file', '*.pdf')]) # show an "Open" dialog box and return the path to the selected file
-
-root.withdraw()
-out_dir = askdirectory()
-
-pdf_sep(path,out_dir)
+if path != '':
+  root.withdraw()
+  out_dir = askdirectory()
+  if out_dir != '':
+    pdf_sep(path,out_dir)
